00170beda79976f21ff6d17333a27a694bd3b399
MingT-L8553 <1071252445@qq.com>
2021-06-08 22:08:19 +0800

    add a function in index.js

--- cloudfunction/getOpenid/index.js
+++ cloudfunction/getOpenid/index.js
@@ -5,7 +5,8 @@ cloud.init()
 
 // 云函数入口函数
 exports.main=async()=>{return cloud.getWXContext()}
-
+//当初写下这串代码时只有我和上帝知道这是干什么的
+//现在只有上帝知道
 
 // exports.main = async (event, context) => {
 //   const wxContext = cloud.getWXContext()
